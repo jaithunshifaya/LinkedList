e3c9494ef50a5d21f05fc96e1fd06637cb36bb3b
jaithunshifaya <jaithunshifaya@gmail.com>
2025-03-27 14:40:30 +0530

    First Commit

---
+++ RemoveNthFromEnd.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode left=head;
+        ListNode right=head;
+        
+        for(int i=0; i<n;i++){
+            right=right.next;
+        }
+        if(right == null){
+            return head.next;
+        }
+        
+        while(right.next!=null){
+            
+            left=left.next;
+            right=right.next;
+        }
+        left.next=left.next.next;
+        return head;
+        
+    }
+}
