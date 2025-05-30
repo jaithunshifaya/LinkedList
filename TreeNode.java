a4dae5b0d07bc9f4332c84ccace26df70ffb0a5d
jaithunshifaya <jaithunshifaya@gmail.com>
2025-05-30 22:34:53 +0530

    Binary Search List

---
+++ TreeNode.java
@@ -0,0 +1,41 @@
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        return convertListToBST(head, null);
+    }
+    
+    private TreeNode convertListToBST(ListNode head, ListNode tail) {
+        if (head == tail) return null;
+        
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != tail && fast.next != tail) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        TreeNode root = new TreeNode(slow.val);
+        
+        
+        root.left = convertListToBST(head, slow);
+        root.right = convertListToBST(slow.next, tail);
+        
+        return root;
+    }
+}
