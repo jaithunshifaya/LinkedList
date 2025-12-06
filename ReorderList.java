015529f81ba4ebc35ff197de8f768e8ff8b41510
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-12-06 12:02:54 +0530

    Update ReorderList.java

--- ReorderList.java
+++ ReorderList.java
@@ -1,7 +1,6 @@
 class Solution {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null) return;
-
         
         ListNode slow = head, fast = head;
         while (fast.next != null && fast.next.next != null) {
@@ -9,24 +8,19 @@ class Solution {
             fast = fast.next.next;
         }
 
-       
         ListNode secondHalf = reverseList(slow.next);
         slow.next = null;  
-
        
         ListNode firstHalf = head;
         while (secondHalf != null) {
             ListNode temp1 = firstHalf.next;
             ListNode temp2 = secondHalf.next;
-
             firstHalf.next = secondHalf;
             secondHalf.next = temp1;
-
             firstHalf = temp1;
             secondHalf = temp2;
         }
     }
-
     
     private ListNode reverseList(ListNode head) {
         ListNode prev = null, curr = head;
